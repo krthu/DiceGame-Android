eeca3d59f0ef4bc384b70f651dc221fe9ad9cd48
KT <thun.kristian@gmail.com>
2023-11-12 09:53:17 +0100

    Working recyclear view

--- app/src/main/java/com/example/dicegame/scoreboardActivity.kt
+++ app/src/main/java/com/example/dicegame/scoreboardActivity.kt
@@ -3,15 +3,28 @@ package com.example.dicegame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class scoreboardActivity : AppCompatActivity() {
     private lateinit var backButton: Button
+    private lateinit var rvScoreItems: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scoreboard)
+        rvScoreItems = findViewById(R.id.rvScoreItems)
         backButton = findViewById(R.id.backButton)
         backButton.setOnClickListener{
             onBackPressedDispatcher.onBackPressed()
         }
+        var scoreItems = mutableListOf(
+            scoreItem("1", 0, false),
+            scoreItem("2", 8, true),
+            scoreItem("3", 0, false),
+            scoreItem("4", 16, true),
+        )
+        val adapter = scoreAdapter(scoreItems)
+        rvScoreItems.adapter = adapter
+        rvScoreItems.layoutManager = LinearLayoutManager(this)
     }
 }
